8b3cfd2be56db5cee731c1e33a390f0d7bbf0812
nilaykulkarni <nilaykulkarni21@gmail.com>
2020-06-05 19:23:24 +0530

    Updates

---
+++ precache-manifest.b17e9f41d2395746ad3c0b2674b54e39.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e99f5b94fe1e7d2c787df097d0b3c3",
+    "url": "/myom-web-app/index.html"
+  },
+  {
+    "revision": "d515738942f1ff9cba19",
+    "url": "/myom-web-app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2ea2550f935926698f04",
+    "url": "/myom-web-app/static/js/2.25c1bccd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myom-web-app/static/js/2.25c1bccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d515738942f1ff9cba19",
+    "url": "/myom-web-app/static/js/main.60d82c90.chunk.js"
+  },
+  {
+    "revision": "affda1dc56fcd49c347c",
+    "url": "/myom-web-app/static/js/runtime-main.801be622.js"
+  },
+  {
+    "revision": "30897ee90eb892fad63d90daf13a5e2e",
+    "url": "/myom-web-app/static/media/blog-post-1.30897ee9.md"
+  },
+  {
+    "revision": "4ced253a3f6dbf41af9c9f11549dd432",
+    "url": "/myom-web-app/static/media/blog-post-2.4ced253a.md"
+  },
+  {
+    "revision": "fec40a74ae34a689100b059cb794fee2",
+    "url": "/myom-web-app/static/media/blog-post-3.fec40a74.md"
+  },
+  {
+    "revision": "f15d60969c9a4eae725a0d4771f93eff",
+    "url": "/myom-web-app/static/media/collectionDrive.f15d6096.jpg"
+  },
+  {
+    "revision": "1b0bd462e10b88229b2d02d5e92615bd",
+    "url": "/myom-web-app/static/media/dog.1b0bd462.jpg"
+  },
+  {
+    "revision": "5fcd63d4c3a0c4d14fb7e0aeddd1500b",
+    "url": "/myom-web-app/static/media/dogShelter.5fcd63d4.png"
+  },
+  {
+    "revision": "32fa54e2f4d87f236b2ef52a0be4a7c0",
+    "url": "/myom-web-app/static/media/featuredPost2.32fa54e2.jpg"
+  },
+  {
+    "revision": "dee2571c7475367041ea82bda63a781d",
+    "url": "/myom-web-app/static/media/logo.dee2571c.png"
+  },
+  {
+    "revision": "508ff63656e6f3fa062a6dcaa91eece9",
+    "url": "/myom-web-app/static/media/myomTeam.508ff636.jpg"
+  },
+  {
+    "revision": "f74ac121eff2fb51f425b454f0b445a6",
+    "url": "/myom-web-app/static/media/oldAgeHome.f74ac121.jpg"
+  },
+  {
+    "revision": "cebcb618db2ea9e2ad77e71f757dcb44",
+    "url": "/myom-web-app/static/media/uss.cebcb618.jpg"
+  }
+]);
